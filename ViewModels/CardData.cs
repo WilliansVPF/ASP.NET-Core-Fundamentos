12295db1f01c2d3f506b66cafb9b8e79d7e18e38
Willians Freitas <williansvpf@gmail.com>
2025-08-08 12:29:46 -0300

    teste partial views

---
+++ ViewModels/CardData.cs
@@ -0,0 +1,8 @@
+namespace TWTodoList.ViewModels;
+
+public class CardData
+{
+    public string Title { get; set; } = string.Empty;
+    public string Subtitle { get; set; } = string.Empty;
+    public string Content { get; set; } = string.Empty;
+}
